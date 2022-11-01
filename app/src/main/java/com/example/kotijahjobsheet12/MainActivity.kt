a3a668f789ea1e2d9740c027475d20f2179a5ec4
Your Name <zahrotun@gmail.com>
2022-11-01 08:47:57 +0700

    first commit

---
+++ app/src/main/java/com/example/kotijahjobsheet12/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.kotijahjobsheet12
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnNext: Button = findViewById(R.id.button)
+        val btnPindah: Button = findViewById(R.id.button2)
+
+
+        btnPindah.setOnClickListener {
+            val intent = Intent(this, MainActivity3 ::class.java)
+            startActivity(intent)
+        }
+
+        btnNext.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
